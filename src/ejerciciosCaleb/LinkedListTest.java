1d199e6d483521a387fc940e8b7e8b66c722a6ab
MalenaLucero <luceromalena@hotmail.com>
2020-09-07 11:26:20 -0300

    queue and stack

--- src/ejerciciosCaleb/LinkedListTest.java
+++ src/ejerciciosCaleb/LinkedListTest.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 public class LinkedListTest {
 	public static void main(String[] args) {
 		LinkedList<String> names = new LinkedList<String>();
+		
+		System.out.println("-----QUEUE-----");
 		names.add("Caleb");
 		names.add("Sue");
 		names.add("Sally");
@@ -17,6 +19,17 @@ public class LinkedListTest {
 		System.out.println(names);
 		System.out.println("names.remove()");
 		names.remove();
+		
+		System.out.println("-----STACK-----");
+		names.push("Caleb");
+		names.push("Sue");
+		names.push("Sally");
+		System.out.println(names);
+		System.out.println("names.pop()");
+		names.pop();
+		System.out.println(names);
+		System.out.println("names.pop()");
+		names.pop();
 		System.out.println(names);
 	}
 }
