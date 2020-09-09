569564e07e12724f09f27e1c81d14cd8043f24f3
MalenaLucero <luceromalena@hotmail.com>
2020-09-08 21:52:27 -0300

    ejercicio ciudades

---
+++ src/ejerciciosAda/EjercicioCiudades.java
@@ -0,0 +1,17 @@
+package ejerciciosAda;
+//
+//En un trayecto entre dos ciudades se deben realizar paradas intermedias. 
+//Se desea conocer cuantas paradas
+//se van a realizar en el viaje y la longitud de los trayectos en kilometros. 
+//Se desea conocer la longitud
+//promedio entre ciudades y 
+//cuales son las paradas mas cercanas
+
+//inicio
+//*------- * ---------------- * ----- * ----------- *
+//   40            100            15      60
+
+
+public class EjercicioCiudades {
+
+}
