deb7736887bc1f0d5552d2f2e06036b738e1cc59
MalenaLucero <luceromalena@hotmail.com>
2020-09-18 18:12:36 -0300

    cambios en ahorcado

--- src/ejerciciosAda/EjercicioAhorcado.java
+++ src/ejerciciosAda/EjercicioAhorcado.java
@@ -6,34 +6,49 @@ public class EjercicioAhorcado {
 	public static final int INTENTOS = 5;
 	
 	public static void main(String[] args) {
+		imprimirTitulo();
 		Scanner sc = new Scanner(System.in);
 		String palabraSecreta = cargarPalabraSecreta(sc);
 		boolean espacios[] = inicializarArrayBooleanos(palabraSecreta);
 		int errores = 0;
+		imprimirMensajeInicial();
 		
 		while(!partidaTerminada(espacios) && errores < INTENTOS) {
 			imprimirProgreso(espacios, palabraSecreta);
 			char letra = cargarLetra(sc);
-			boolean letraIncluida = procesarLetra(letra, espacios, palabraSecreta);
-			if(!letraIncluida) errores++;
-			imprimirIntentosRestantes(letraIncluida, errores);
+			boolean letraIncluida = palabraSecreta.indexOf(letra) != -1;
+			if(letraIncluida) {
+				procesarLetra(letra, espacios, palabraSecreta);
+				System.out.println("Muy bien!");
+			} else {
+				errores++;
+				imprimirIntentosRestantes(errores);
+			}
 		}
-		
 		imprimirEstado(errores);
 		imprimirPalabraSecreta(palabraSecreta);
-		
+	}
+	
+	private static void imprimirMensajeInicial() {
+		System.out.println();
+		System.out.println("Tenés " + INTENTOS + " intentos");
+		System.out.println("Empezamos!");
 	}
 
-	private static void imprimirIntentosRestantes(boolean letraIncluida, int errores) {
+	private static void imprimirTitulo() {
+		String titulo = "Bienvenido al juego del ahorcado!";
+		dibujarTrayecto(titulo.length(), "~");
 		System.out.println();
-		if(letraIncluida) {
-			System.out.println("Muy bien!");
-		} else {
-			System.out.println("Ño :(");
-		}
+		System.out.println(titulo.toUpperCase());
+		dibujarTrayecto(titulo.length(), "~");
+		System.out.println();
+	}
+
+	private static void imprimirIntentosRestantes(int errores) {
 		int intentosRestantes = INTENTOS - errores;
-		System.out.println("Te quedan " + intentosRestantes + " intentos");
 		System.out.println();
+		System.out.println("No :(");
+		System.out.println("Te quedan " + intentosRestantes + " intentos");
 	}
 
 	private static void imprimirPalabraSecreta(String palabraSecreta) {
@@ -50,36 +65,30 @@ public class EjercicioAhorcado {
 		}
 	}
 
-	private static boolean procesarLetra(char letra, boolean[] espacios, String palabraSecreta) {
-		boolean letraIncluida = false;
+	private static void procesarLetra(char letra, boolean[] espacios, String palabraSecreta) {
 		for(int i=0; i<palabraSecreta.length(); i++) {
 			if(palabraSecreta.charAt(i) == letra) {
 				espacios[i] = true;
-				letraIncluida = true;
 			}
 		}
-		return letraIncluida;
 	}
 
 	private static char cargarLetra(Scanner sc) {
-		System.out.println("Ingresar una letra:");
-		String letraString = sc.next();
-		char letraChar = letraString.charAt(0);
-		return letraChar;
+		System.out.println("Ingresá una letra:");
+		String letra = sc.next();
+		return letra.charAt(0);
 	}
 
 	private static boolean partidaTerminada(boolean[] espacios) {
-		boolean terminado = true;
 		for(boolean b:espacios) {
-			if(!b) terminado = false; 
+			if(!b) return false;
 		}
-		return terminado;
+		return true;
 	}
 
 	private static String cargarPalabraSecreta(Scanner sc) {
-		System.out.println("Ingrese la palabra secreta:");
-		String palabra = sc.next();
-		return palabra;
+		System.out.println("Ingresá la palabra secreta:");
+		return sc.next();
 	}
 
 	private static boolean[] inicializarArrayBooleanos(String palabraSecreta) {
@@ -89,6 +98,7 @@ public class EjercicioAhorcado {
 	}
 
 	private static void imprimirProgreso(boolean[] espacios, String palabraSecreta) {
+		System.out.println();
 		for(int i=0; i<espacios.length; i++) {
 			if(espacios[i]) {
 				System.out.print(palabraSecreta.charAt(i) + " "); 
@@ -99,9 +109,10 @@ public class EjercicioAhorcado {
 		System.out.println();
 		System.out.println();
 	}
-
-	private static void imprimirArray(boolean[] arr) {
-		for(boolean b: arr) System.out.print(b + " ");
-		System.out.println();
+	
+	private static void dibujarTrayecto(int longitud, String simbolo) {
+		for(int i=0; i<longitud; i++) {
+			System.out.print(simbolo);
+		}
 	}
 }
